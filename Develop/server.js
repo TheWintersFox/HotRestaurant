7bc5510163307a89248f7926bdd8896f5b755f93
Luke Evans <lukeevanstax@gmail.com>
2020-07-25 13:32:52 -0400

    updating server file

--- Develop/server.js
+++ Develop/server.js
@@ -47,38 +47,40 @@ var tables = [{
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/index", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/waitlist", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
+app.get("/api/waitlist", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 // Displays all table
 app.get("/api/tables", function(req, res) {
-  return res.json(tables);
+    return res.json(tables);
 });
 
 
 // Create New Characters - takes in JSON input
 app.post("/api/table", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newTable = req.body;
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newTables = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newTables.routeName = newTables.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newCharacter);
+    console.log(newTables);
 
-  tables.push(newCharacter);
+    tables.push(newTables);
 
-  res.json(newCharacter);
+    res.json(newTables);
 });
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
+
+
