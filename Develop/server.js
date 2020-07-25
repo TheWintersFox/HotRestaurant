25902fb544bddf5b745a86aed4bd4579759b0630
Luke Evans <lukeevanstax@gmail.com>
2020-07-25 12:58:58 -0400

    updating server file to display tables

--- Develop/server.js
+++ Develop/server.js
@@ -22,56 +22,70 @@ var PORT = 3000;
 // Data
 // ===========================================================
 var characters = [{
-  routeName: "yoda",
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
 }, {
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
 }, {
-  routeName: "obiwankenobi",
-  name: "Obi Wan Kenobi",
-  role: "Jedi Master",
-  age: 55,
-  forcePoints: 1350
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
 }];
 
 // Routes
 // ===========================================================
 
 app.get("/", function(req, res) {
-  res.send("Welcome to Hot Restaurant!");
+    res.send("Welcome to Hot Restaurant!");
 });
 
 // What does this route do?
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
+app.get("/api/tables", function(req, res) {
+    return res.json(characters);
 });
 
 // What does this route do?
-app.get("/api/characters/:character", function(req, res) {
-  // What does this code do?
-  var chosen = req.params.character;
-  console.log(chosen);
+app.get("/api/tables", function(req, res) {
+    // What does this code do?
+    var chosen = req.params.character;
+    console.log(chosen);
 
-  // What does this code do?
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
+    // What does this code do?
+    for (var i = 0; i < characters.length; i++) {
+        if (chosen === characters[i].routeName) {
+            return res.json(characters[i]);
+        }
     }
-  }
 
-  // What does this code do?
-  return res.send("No character found");
+    // What does this code do?
+    return res.send("No character found");
 });
 
 // Listener
 // ===========================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+  console.log(newtables);
+
+  // We then add the json the user sent to the tables array
+  tables.push(newtables);
+
+  // We then display the JSON to the users
+  res.json(newtables);
+});
+
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
